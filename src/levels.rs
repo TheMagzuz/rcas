f92f13b48c0c4bbd1dc1c807745b0e3e1aee3127
Markus Dam <markus.dam123@gmail.com>
2022-10-17 22:59:43 +0200

    Implement loading times from save files

---
+++ src/levels.rs
@@ -0,0 +1,98 @@
+use anyhow::anyhow;
+
+#[derive(Hash, Eq, PartialEq)]
+pub enum Chapter {
+    Prologue, City(Side), Site(Side), Resort(Side), Ridge(Side), Temple(Side), Reflection(Side), Summit(Side), Epilogue, Core(Side), Farewell
+}
+
+impl Chapter {
+    pub fn from_index(index: u8, side: Side) -> anyhow::Result<Self> {
+        match index {
+            0 => Ok(Self::Prologue),
+            1 => Ok(Self::City(side)),
+            2 => Ok(Self::Site(side)),
+            3 => Ok(Self::Resort(side)),
+            4 => Ok(Self::Ridge(side)),
+            5 => Ok(Self::Temple(side)),
+            6 => Ok(Self::Reflection(side)),
+            7 => Ok(Self::Summit(side)),
+            8 => Ok(Self::Epilogue),
+            9 => Ok(Self::Core(side)),
+            10 => Ok(Self::Farewell),
+            _ => Err(anyhow!("Invalid chapter index: {}", index)),
+        }
+    }
+    
+    pub fn to_string(&self) -> String {
+        match self {
+            Self::Prologue | Self::Epilogue | Self::Farewell => self.long_name().to_owned(),
+            chapter @ (Self::City(side) | Self::Site(side) | Self::Resort(side) | Self::Ridge(side) | Self::Temple(side) | Self::Reflection(side) | Self::Summit(side) | Self::Core(side)) => chapter.short_name().to_owned() + side.to_string(),
+        }
+    }
+
+    pub fn short_name(&self) -> &str {
+        match self {
+            Self::Prologue => "Prologue",
+            Self::City(_) => "1",
+            Self::Site(_) => "2",
+            Self::Resort(_) => "3",
+            Self::Ridge(_) => "4",
+            Self::Temple(_) => "5",
+            Self::Reflection(_) => "6",
+            Self::Summit(_) => "7",
+            Self::Epilogue => "Epilogue",
+            Self::Core(_) => "8",
+            Self::Farewell => "9"
+        }
+    }
+
+    pub fn long_name(&self) -> &str {
+        match self {
+            Self::Prologue => "Prologue",
+            Self::City(_) => "Forsaken City",
+            Self::Site(_) => "Old Site",
+            Self::Resort(_) => "Celestial Resort",
+            Self::Ridge(_) => "Golden Ridge",
+            Self::Temple(_) => "Mirror Temple",
+            Self::Reflection(_) => "Reflection",
+            Self::Summit(_) => "The Summit",
+            Self::Epilogue => "Epilogue",
+            Self::Core(_) => "Core",
+            Self::Farewell => "Farewell"
+        }
+    }
+}
+
+#[derive(Hash, Eq, PartialEq)]
+pub enum Side {
+    A, B, C
+}
+
+impl Side {
+    pub fn from_index(index: u8) -> anyhow::Result<Self> {
+        match index {
+            0 => Ok(Self::A),
+            1 => Ok(Self::B),
+            2 => Ok(Self::C),
+            _ => Err(anyhow!("Invalid side index: {}", index)),
+        }
+    }
+    pub fn to_string(&self) -> &str {
+        match self {
+            Self::A => "A",
+            Self::B => "B",
+            Self::C => "C",
+        }
+    }
+}
+
+pub const ANY_PERCENT_ROUTE: [Chapter; 8] = [
+    Chapter::Prologue,
+    Chapter::City(Side::A),
+    Chapter::Site(Side::A),
+    Chapter::Resort(Side::A),
+    Chapter::Ridge(Side::A),
+    Chapter::Temple(Side::A),
+    Chapter::Reflection(Side::A),
+    Chapter::Summit(Side::A),
+];
---
+++ src/saves.rs
@@ -0,0 +1,87 @@
+use std::{fs::File, io::BufReader};
+use std::collections::HashMap;
+use std::time::Duration;
+use anyhow::anyhow;
+use quick_xml::events::BytesStart;
+use quick_xml::{events::Event, reader::Reader, name::QName};
+
+use crate::levels::{Chapter, Side};
+
+
+pub fn load_save(path: &str) -> anyhow::Result<HashMap<Chapter, AreaModeStats>> {
+    let mut data = HashMap::new();
+    let file = File::open(path)?;
+    let buf_reader = BufReader::new(file);
+    let mut reader = Reader::from_reader(buf_reader);
+    reader.trim_text(true);
+
+    let mut buf = Vec::new();
+    let mut side_index = 0;
+    let mut chapter_index: Option<u8> = None;
+
+    loop {
+        let event = reader.read_event_into(&mut buf)?;
+        match event {
+            Event::Eof => break,
+            Event::Start(tag) => {
+                match tag.name().as_ref() {
+                    b"AreaStats" => {
+                        side_index = 0;
+                        let chapter = find_attr(b"ID", &tag)?;
+                        chapter_index = Some(chapter.parse::<u8>()?);
+                    }
+                    b"AreaModeStats" => {
+                        let chapter_index = chapter_index.ok_or(anyhow!("Reached an AreaModeStats tag without a chapter index being set"))?;
+                        let chapter = Chapter::from_index(chapter_index, Side::from_index(side_index)?)?;
+                        side_index += 1;
+
+                        let time_played = find_attr(b"TimePlayed", &tag)?.parse::<u64>()?;
+                        if time_played == 0 {
+                            continue;
+                        }
+                        let best_time = find_attr(b"BestTime", &tag)?.parse::<u64>()?;
+
+                        data.insert(chapter, AreaModeStats::from_u64(time_played, best_time));
+                    }
+                    _ => (),
+                }
+            },
+            _ => (),
+        }
+    }
+    buf.clear();
+    Ok(data)
+}
+
+fn find_attr(name: &[u8], tag: &BytesStart) -> anyhow::Result<String> {
+    let target_name = QName(name);
+
+    let attr = tag.attributes().find(|a| {
+        if let Ok(a) = a {
+            a.key == target_name
+        } else {
+            false
+        }
+    });
+
+    if let Some(attr) = attr {
+        return Ok(std::str::from_utf8(attr?.value.as_ref())?.to_owned());
+    } else {
+        return Err(anyhow!("Could not find attribute {} on tag {}", std::str::from_utf8(name).unwrap(), std::str::from_utf8(tag.name().as_ref()).unwrap()));
+    }
+}
+
+#[derive(Debug)]
+pub struct AreaModeStats {
+    pub time_played: Duration,
+    pub best_time: Duration,
+}
+
+impl AreaModeStats {
+    pub fn from_u64(time_played: u64, best_time: u64) -> Self {
+        Self {
+            time_played: Duration::from_micros(time_played/10),
+            best_time: Duration::from_micros(best_time/10),
+        }
+    }
+}
