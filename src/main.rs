f3aea0b176c8910f71d7210f17c432841a67d78d
Markus Dam <markus.dam123@gmail.com>
2022-10-20 16:13:39 +0200

    Start watching the save file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
+use std::{path::Path, collections::HashMap};
+
 use anyhow::Result;
+
+use levels::Chapter;
+use saves::AreaModeStats;
+use watch::AsyncWatcher;
 
 mod levels;
 mod saves;
+mod watch;
 
 fn main() -> Result<()> {
-    let path = shellexpand::full("$XDG_DATA_HOME/Celeste/Saves/2.celeste")?;
-    let path = std::path::Path::new(path.as_ref());
-    println!("{:?}", path);
-    let save = saves::load_save(&path)?;
+    let path_str = shellexpand::full("$XDG_DATA_HOME/Celeste/Saves/2.celeste")?;
+    let path_str = path_str.as_ref();
+    let path = Path::new(path_str);
+
+    let mut watcher = AsyncWatcher::new(path)?;
+
+    futures::executor::block_on(async {
+        watcher.watch().await;
+    });
+
+    Ok(())
+}
+
+pub fn print_times(save: &HashMap<Chapter, AreaModeStats>) {
     for key in levels::ANY_PERCENT_ROUTE {
         println!("{:?}: {:?}", key.to_string(), save[&key]);
     }
-    Ok(())
 }
